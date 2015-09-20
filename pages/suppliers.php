31c1382b8519dd792ea1c49e6e8bfcb17c539906
Michael Joslyn <michaeljoslyn@me.com>
2015-09-20 12:09:45 +0800

    no message

--- pages/suppliers.php
+++ pages/suppliers.php
@@ -402,7 +402,7 @@
   		<select class="form-control" name="currency">
    		       <option>1</option>
     	       <option>2</option>
-   		       <option>5</option>
+   		       <option>3</option>
     	       <option>4</option>
         </select> 
         
